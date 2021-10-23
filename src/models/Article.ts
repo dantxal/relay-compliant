caa1dbba0aa863e2c98731b0bb5bd065b2a4fe1c
dantxal <dantxal@gmail.com>
2021-10-23 06:58:45 -0300

    Adds ApplyCursor to Article model

--- src/models/Article.ts
+++ src/models/Article.ts
@@ -1,52 +1,76 @@
-import { FindCursor, MongoClient, Document } from "mongodb";
+import {
+  FindCursor, MongoClient, Document, ObjectId,
+} from 'mongodb';
 
 type PaginationParameters = {
-	first?: number
-	last?: number
+	first?: number,
+	last?: number,
+	before?: number,
+	after?: number,
 }
 
-export async function getArticles(mongodb: MongoClient, { first, last }: PaginationParameters) {
-	const query = mongodb.db('relay-compliant').collection('Articles').find();
-	const pageInfo = await applyPagination(query, first, last);
+export async function getArticles(mongodb: MongoClient, {
+  first, last, before, after,
+}: PaginationParameters) {
+  const query = applyCursor(
+    mongodb.db('relay-compliant').collection('Articles').find(),
+    before,
+    after,
+  );
+  const pageInfo = await applyPagination(query, first, last);
 
-	return {
-		query,
-		pageInfo
-	}
+  return {
+    query,
+    pageInfo,
+  };
+}
+
+function applyCursor(query: FindCursor<Document>, before: any, after: any) {
+  const filter: Document = {
+    _id: {},
+  };
+  if (before) {
+    filter._id.$lt = new ObjectId(before.value);
+  }
+  if (after) {
+    filter._id.$gt = new ObjectId(after.value);
+  }
+
+  return query.filter(filter);
 }
 
 async function applyPagination(query: FindCursor<Document>, first?: number, last?: number) {
-	let count;
+  let count = 0;
 
-	if (first || last) {
-		count = await query.clone().count();
-		let limit = 0;
-		let skip = 0;
+  if (first || last) {
+    count = await query.clone().count();
+    let limit = 0;
+    let skip = 0;
 
-		if(first && count > first) {
-			limit = first;
-		}
+    if (first && count > first) {
+      limit = first;
+    }
 
-		if(last) {
-			if(limit && limit > last) {
-				skip = limit - last;
-				limit = limit - skip;
+    if (last) {
+      if (limit && limit > last) {
+        skip = limit - last;
+        limit -= skip;
       } else if (!limit && count > last) {
         skip = count - last;
       }
-		}
+    }
 
-		if (skip) {
+    if (skip) {
       query.skip(skip);
     }
 
     if (limit) {
       query.limit(limit);
     }
+  }
 
-		return {
-			hasNextPage: Boolean(first && count > first),
-			hasPreviousPage: Boolean(last && count > last),
-		}
-	}
+  return {
+    hasNextPage: Boolean(first && count > first),
+    hasPreviousPage: Boolean(last && count > last),
+  };
 }
